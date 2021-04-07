e47af372016f938582dbb038a825ffa86f6025a7
JElmanoAF <joao515515@hotmail.com>
2021-04-07 17:12:09 +0100

    Correção de erros

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-let express = require('express')
+let express = require('express');
 //import body parser
-let bodyParser = require(‘body-parser’);
+let bodyParser = require('body-parser');
 //import mongoose
-let mongoose = require(‘mongoose’);
+let mongoose = require('mongoose');
 let app = express();
 //Import routes
-let apiRoutes = require(“./RegistoRoutes”)
+let apiRoutes = require("./RegistoRoutes")
 //Configurar bodyparser para processar pedidos
 app.use(bodyParser.urlencoded({
 extended: true
 }));
 app.use(bodyParser.json());
 //Ligar à BD
-const dbPath = ‘mongodb://localhost/bioApp’;
+const dbPath = 'mongodb://localhost/bioApp';
 const options = {useNewUrlParser: true, useUnifiedTopology: true}
 const mongo = mongoose.connect(dbPath, options);
 mongo.then(() => {
-console.log(‘Ligado à BD’);
+console.log('Ligado à BD');
 }, error => {
-console.log(error, ‘error’);
+console.log(error, 'error');
 });
 var db = mongoose.connection;
 //Verificar Ligação
 if (!db)
-console.log(“Error connecting db”);
+console.log("Error connecting db");
 else
-console.log(“DB Connected Successfully”);
+console.log("DB Connected Successfully");
 // Porto Servidor
 var port = process.env.PORT || 6666;
 //Usar API routes na app
-app.use(‘/api’, apiRoutes)
+app.use('/api', apiRoutes)
 // Iniciar Servidor
 app.listen(port, function() {
-console.log(“Servidor UP no porto: “+ port);
+console.log("Servidor UP no porto: "+ port);
 });
